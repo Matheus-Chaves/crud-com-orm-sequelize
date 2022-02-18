604eefdb14321d9c4360a0dd80ae0d4eef4d881a
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-02-18 20:01:27 -0300

    adicionando Soft Delete, novas rotas e migrations

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 const pessoas = require("./pessoasRoute");
+const niveis = require("./niveisRoute");
+const turmas = require("./turmasRoute");
 
 module.exports = (app) => {
-  app.use(pessoas);
+  app.use(pessoas, niveis, turmas);
 };
