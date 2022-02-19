46deef4c749f911847b36df0a414d90c8a89c297
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-02-19 15:42:07 -0300

    adicionando escopos e validações

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -18,15 +18,39 @@ module.exports = (sequelize, DataTypes) => {
   }
   Pessoas.init(
     {
-      nome: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          funcaoValidadora: function (dado) {
+            if (dado.length < 3)
+              throw new Error("O campo nome deve ter mais que 3 caracteres!");
+          },
+        },
+      },
       ativo: DataTypes.BOOLEAN,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "E-mail inválido.",
+          },
+        },
+      },
       role: DataTypes.STRING,
     },
     {
       sequelize,
       modelName: "Pessoas",
       paranoid: true,
+      defaultScope: {
+        //Define um escopo padrão, será aplicado nas queries
+        where: { ativo: true },
+      },
+      scopes: {
+        //define escopos que precisam ser chamados para serem utilizados
+        todos: { where: {} },
+      },
     }
   );
   return Pessoas;
